cc231db4dd6d93d379696c161568c296320b9132
Baxter <baxterjharris@gmail.com>
2021-11-28 19:40:54 -0700

    added oauth2

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '229405922610197',
+        clientSecret: 'f47743cc762282c95029e1f4fc9b750e'
+    }
 }
